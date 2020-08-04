2f8c946a544cefc81a2f38e5310a67fcada2b30d
Rinny <rinny.mamachan073@gmail.com>
2020-08-04 17:12:43 +0530

    Updates

---
+++ precache-manifest.9f329f3c43d38471a0dc85e5af03cddb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b243b0e25e72956fc467f8bc03f83fe3",
+    "url": "/Rinny073/Robofriends/index.html"
+  },
+  {
+    "revision": "1dee4ab3cc1523e26120",
+    "url": "/Rinny073/Robofriends/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cec0406f321b279360e3",
+    "url": "/Rinny073/Robofriends/static/css/main.88adb929.chunk.css"
+  },
+  {
+    "revision": "1dee4ab3cc1523e26120",
+    "url": "/Rinny073/Robofriends/static/js/2.342815a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rinny073/Robofriends/static/js/2.342815a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cec0406f321b279360e3",
+    "url": "/Rinny073/Robofriends/static/js/main.b7f71fc3.chunk.js"
+  },
+  {
+    "revision": "4452683a947fa9583723",
+    "url": "/Rinny073/Robofriends/static/js/runtime-main.caa3bdba.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Rinny073/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
